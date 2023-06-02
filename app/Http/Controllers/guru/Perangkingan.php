dd0816f8c66e8884ac1e484af294fe5354d6560b
Ridho Arif Wicaksono <56249846+arifmalabar@users.noreply.github.com>
2023-06-02 11:26:10 +0700

    Perangkingan Gwe Metode

--- app/Http/Controllers/guru/Perangkingan.php
+++ app/Http/Controllers/guru/Perangkingan.php
@@ -13,6 +13,7 @@ class Perangkingan extends Controller
     private DataJenisKriteria $dataJenisKriteria;
     private PelanggaranController $pelanggaranController;
     private SiswaController $siswaController;
+
     public function __construct()
     {
         $this->dataJenisKriteria = new DataJenisKriteria();
@@ -20,43 +21,82 @@ class Perangkingan extends Controller
         $this->pelanggaranController = new PelanggaranController();
         $this->hitJmlKriteria();
     }
+
     public function index()
     {
-        $this->jmlKepetingan();
+        $this->getBenefit(1);
     }
+
     private function hitJmlKriteria()
     {
         $hasil = 0;
-        foreach ($this->dataJenisKriteria->getDataJeniSKriteria() as $jk)
-        {
+        foreach ($this->dataJenisKriteria->getDataJeniSKriteria() as $jk) {
             $hasil += $jk->bobot_poin;
         }
         return $hasil;
     }
+
     private function hitKepentingan($jmlBobot)
     {
         $hasil = $jmlBobot / $this->hitJmlKriteria();
         return $hasil;
     }
+
     private function jmlKepetingan()
     {
         $hasil = 0.0;
-        foreach ($this->dataJenisKriteria->getDataJeniSKriteria() as $jk)
-        {
+        foreach ($this->dataJenisKriteria->getDataJeniSKriteria() as $jk) {
             $hasil += $this->hitKepentingan($jk->bobot_poin);
         }
         return $hasil;
     }//sampai sini dulu boss ngantuk
+
     private function getBenefit($poin_pelanggaran)
     {
-        foreach ($this->siswaController->getDataWithoutID() as $s)
+        $data = array(
+            [
+                "nama_siswa" =>"Ridho Arif", //datanya ambil dari tb_siswa
+                "nilai"=> [ //datanya ambil dari tb_pelanggaran
+                    ["benefit" => "20"],
+                    ["benefit" => "290"],
+                    ["benefit" => "200"]
+                ],
+                "max_data" => "200",
+                "hasil_normalisasi" => [],
+            ],
+            [
+                "nama_siswa" =>"Ludit", //datanya ambil dari tb_siswa
+                "nilai"=> [ //datanya ambil dari tb_pelanggaran
+                    ["benefit" => "210"],
+                    ["benefit" => "90"],
+                    ["benefit" => "700"]
+                ],
+                "max_data" => 0,
+                "hasil_normalisasi" => [],
+            ]
+        );
+        for ($s = 0; $s < count($data); $s++)
         {
-            $min = 0;
-            foreach ($s->data_pelanggar as $dp)
-            {
-
+            $max =0;
+            echo $data[$s]['nama_siswa'];
+            foreach ($data[$s]['nilai'] as $n){
+                echo " ".$n['benefit']." ";
+                if($n['benefit'] > $max){
+                    $max = $n['benefit'];
+                }
             }
+            /*foreach ($data[$s]['nilai'] as $n){
+                $s['hasil_normalisasi'][$n] = $n['benefit'][] / $max;
+            }
+            //echo "<br> Nilai Maks: ".$max;
+            echo "<br>";
+            for ($i = 0; $i < count($s['hasil_normalisasi']); $i++) {
+                echo $s['hasil_normalisasi'][$i]." ";
+            }*/
+
+            //dd($s);
         }
+
     }
     private function dividerVariabel()
     {
